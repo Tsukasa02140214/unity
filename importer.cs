55d338924e437b62c88e630a4727e95eba6b1bb1
Tsukasa02140214 <42490124+Tsukasa02140214@users.noreply.github.com>
2018-08-18 18:24:50 +0900

    Add files via upload

---
+++ importer.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+
+
+//Editorフォルダ 入れる
+public class importer : AssetPostprocessor
+{
+    //Unityに画像をインポートした時呼び出し
+    void OnPostprocessTexture(Texture2D texture)
+    {
+        //Texture設定
+        TextureImporter importer = assetImporter as TextureImporter;
+
+        // null ではない場合
+        if (importer != null)
+        {
+            //Pathに .pngが含まれてれいれば
+            if (assetPath.Contains(".png"))
+            {
+                //TextureType Sprite
+                importer.textureType = TextureImporterType.Sprite;
+                //maxサイズ 128 
+                importer.maxTextureSize = 128;
+                //設定保存
+                importer.SaveAndReimport();
+            }
+        }
+    }
+
+}
